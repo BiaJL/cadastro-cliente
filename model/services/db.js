2b59b9a65fe31b6e6d29c351de7d648b5e44156f
BiaJL <bianca.lima13@fatec.sp.gov.br>
2022-10-19 11:32:38 -0300

    pqp

--- model/services/db.js
+++ model/services/db.js
@@ -16,3 +16,29 @@ async function selectUsuario(){
     const [row] = await conn.query('SELECT * FROM usuario;');
 }
 module.exports = {selectUsuario}
+
+async function insertUsuario(usuario){
+    const conn = await connect();
+    const sql = 'INSERT INTO usuario(nome,senha) VALUES (?,?);';
+    const values = [usuario.nome, usuario.senha];
+    return await conn.query(sql, values);
+}
+
+module.exports = {selectUsuario, insertUsuario}
+
+async function deleteUsuario(id){
+    const conn = await connect();
+    const sql = 'DELETE FROM usuario where id=?;';
+    return await conn.query(sql, [id]);
+}
+
+module.exports = {selectUsuario, insertUsuario, deleteUsuario}
+
+async function updateUsuario(id, usuario){
+    const conn = await connect();
+    const sql = 'UPDATE usuario SET nome=?, senha=? WHERE id=?';
+    const values = [usuario.nome, usuario.senha, id];
+    return await conn.query(sql, values);
+}
+
+module.exports = {selectUsuario, insertUsuario, deleteUsuario, updateUsuario}
